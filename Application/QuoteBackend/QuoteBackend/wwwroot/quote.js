6974b6ede65fa142cf45f209e895c07b95dec32e
DESKTOP-1QO8RDL\123 <meow122@icloud.com>
2018-08-16 21:46:13 +0500

    Теперь цитаты выводятся из бд

--- Application/QuoteBackend/QuoteBackend/wwwroot/quote.js
+++ Application/QuoteBackend/QuoteBackend/wwwroot/quote.js
@@ -1,6 +1,11 @@
 "use strict";
 
 var serverURL = "/api/values/";
+var serverAnswer = {
+  quotes: "null",
+  categories: "null"
+};
+
 
 function GetXMLHttpRequest() {
 
@@ -37,16 +42,26 @@ function GetRequest() {
     }
 
     request.onreadystatechange = function () {
-
         //Надо сдеать обработчик
-
         if (request.readyState == 4) {
             //Передаем управление обработчику пользователя
-            document.getElementById('cas9').innerHTML = request.responseText;
+            serverAnswer = JSON.parse(request.responseText,function(key, value) {
+                if (key == 'CreateDate') return new Date(value);
+                return value;
+              });
+
+
+            if(serverAnswer.quotes.length > 0){
+              data.quotesIsExist = true;
+            }
+
+            data.quotes = serverAnswer.quotes;
+            data.categories = serverAnswer.categories;
+
 
         } else {
             //Оповещаем пользователя о загрузке
-            document.getElementById('cas9').innerHTML = "Get change status..." + request.readyState;
+            // document.getElementById('cas9').innerHTML = "Get change status..." + request.readyState;
         }
 
     }
@@ -142,3 +157,43 @@ function DeleteRequest(id) {
 }
 
 // -----------------------------------------------------------------------------------------------------
+
+GetRequest();
+
+var data = {};
+data.quotesIsExist = false;
+data.quotes = null;
+data.categories = null;
+
+var vm = new Vue({
+  el: '#quotes-wrap',
+  data : data
+});
+
+Vue.component("quote-block",{
+  props: ['quote'],
+  template:`
+    <section class="quote">
+      <span class="quote-id section-second-text">#<span>{{quote.QuoteId}}</span>
+      </span>
+      <span class="quote-separator section-second-text">|</span>
+      <span class="quote-category section-second-text">{{quote.Category}}</span>
+      <span class="quote-img-buttons-wrap">
+        <img class="quote-img-button" src="img/edit.png" alt="Edit"
+        onmouseout="this.src='img/edit.png'"
+        onmouseover="this.src='img/edit-hover.png'"
+        ondragstart="return false"><img
+        class="quote-img-button" src="img/delete.png" alt="Delete"
+        onmouseout="this.src='img/delete.png'"
+        onmouseover="this.src='img/delete-hover.png'"
+        ondragstart="return false">
+      </span>
+      <blockquote class="quote-text section-primary-text">
+        "<span>{{quote.Text}}</span>"
+      </blockquote>
+      <span class="quote-date section-second-text">{{quote.CreateDate.getDate()}}/{{quote.CreateDate.getMonth() + 1}}/{{quote.CreateDate.getFullYear()}}</span>
+      <span class="quote-author section-primary-text">{{quote.Author}}</span>
+    </section>
+  `
+
+});
